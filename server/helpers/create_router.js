01a1e55f444fd3c4ac87e32d529f5227e84662cd
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-05 11:29:54 +0100

    index route works

---
+++ server/helpers/create_router.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const createRouter = function (collection) {
+
+  const router = express.Router();
+
+  // INDEX
+  router.get('/', (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then(docs => res.json(docs))
+      .catch(err => {
+         console.error(err);
+         res.status(500);
+         res.json({ status: 500, error: err })
+      });
+  });
+
+  // CREATE
+
+  // DELETE
+  
+  return router
+
+}
+
+module.exports = createRouter;
