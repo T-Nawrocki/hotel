5d61ec5c8f40aea4bfec7e135267b4d300a22688
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-05 13:35:46 +0100

    fetching bookings list from server works

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <booking-form />
+    <bookings-list />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BookingForm from "./components/BookingForm";
+import BookingsList from "./components/BookingsList";
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    "booking-form": BookingForm,
+    "bookings-list": BookingsList
   }
 }
 </script>
---
+++ client/src/components/BookingsList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="bookings-list">
+    <h1>Active Bookings</h1>
+    <ul>
+      <booking-item v-for="(booking, index) in bookings"
+                    :booking="booking"
+                    :key="index" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import BookingsService from "../services/BookingsService.js";
+import BookingItem from "./BookingItem.vue"
+
+export default {
+  name: "BookingsList",
+
+  components: {
+    "booking-item": BookingItem
+  },
+
+  data () {
+    return {
+      bookings: []
+    }
+  },
+
+  mounted () {
+    // fetch bookings list from server
+    BookingsService.getBookings()
+      .then(data => this.bookings = data);
+  }
+}
+</script>
+
+<style>
+
+</style>
